50d5b66c8af5992dc2582471d1a6ed61757728d7
Ftnt <63697694+Ftnt@users.noreply.github.com>
2022-04-22 01:21:03 -0500

    feat: function to convert array to object

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const { chromium } = require("playwright");
 const URL =
   "https://www.sii.cl/servicios_online/1047-nomina_inst_financieras-1714.html";
 
+const arrayToObject = (array) => {
+  return array.map((dato) => {
+    return {
+      id: dato[0],
+      razon_social: dato[1],
+      pais: dato[2],
+      datos_inscripcion: dato[3],
+      vigencia_hasta: dato[4],
+      datos_ultima_actualizacion: dato[5],
+      estado: dato[6],
+    };
+  });
+};
+
 (async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
@@ -15,7 +29,7 @@ const URL =
     });
   });
 
-  console.log(allData);
+  console.log(arrayToObject(allData));
 
   await browser.close();
 })();
